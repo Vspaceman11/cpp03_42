0b9194ba9c34545161fd5610e308dcaa0d88d4b8
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2026-01-16 16:45:30 +0100

    Ad virual to destructors

--- ex02/ScavTrap.hpp
+++ ex02/ScavTrap.hpp
@@ -8,7 +8,7 @@ class ScavTrap : public ClapTrap
 		ScavTrap(const ScavTrap& other);
 		ScavTrap& operator=(const ScavTrap& other);
 
-		~ScavTrap();
+		virtual ~ScavTrap();
 
 		void attack(const std::string& target);
 		void guardGate();
