ad25f4671c7d6ab02f6e444e3b799070bd0e2d17
Vspaceman11 <pushkarov.vladyslav@gmail.com>
2026-01-16 13:07:42 +0100

    Copy everything from ex02

---
+++ ex03/ClapTrap.cpp
@@ -0,0 +1,79 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap() :
+				_name(""),
+				_hitPoints(10),
+				_energyPoints(10),
+				_attackDamage(0)
+{
+	std::cout << "Default Constructor ClapTrap " << _name << " called" << std::endl;
+}
+ClapTrap::ClapTrap(const std::string& name) :
+				_name(name),
+				_hitPoints(10),
+				_energyPoints(10),
+				_attackDamage(0)
+{
+	std::cout << "Parametrized Constructor ClapTrap " << _name << " called" << std::endl;
+}
+ClapTrap::ClapTrap(const ClapTrap& other)
+{
+	std::cout << "Copy constructor ClapTrap " << _name << " called" << std::endl;
+	*this = other;
+}
+ClapTrap& ClapTrap::operator=(const ClapTrap& other)
+{
+	std::cout << "Copy assignment operator ClapTrap " << _name << " called" << std::endl;
+	if (this != &other)
+	{
+		this->_name = other._name;
+		this->_hitPoints = other._hitPoints;
+		this->_energyPoints = other._energyPoints;
+		this->_attackDamage = other._attackDamage;
+	}
+	return *this;
+}
+
+ClapTrap::~ClapTrap()
+{
+	std::cout << "Destructor ClapTrap " << _name << " called" << std::endl;
+}
+
+void ClapTrap::attack(const std::string& target)
+{
+	if (_hitPoints == 0 || _energyPoints == 0)
+	{
+		std::cout << "ClapTrap " << _name << " Cannot attack. No energy or no HP" << std::endl;
+		return;
+	}
+	_energyPoints -= 1;
+	std::cout << "ClapTrap " << _name << " attacks " << target << ", causing " << _attackDamage << " points of damage!" << std::endl;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+	if (_hitPoints == 0)
+	{
+		std::cout << "ClapTrap " << _name << " is dead and cannot take damage" << std::endl;
+		return;
+	}
+	if (amount >= _hitPoints)
+		_hitPoints = 0;
+	else
+		_hitPoints -= amount;
+
+	std::cout << "ClapTrap " << _name << " takes " << amount << " damage, and leaving " << _hitPoints << " HP!" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+	if (_hitPoints == 0 || _energyPoints == 0)
+	{
+		std::cout << "ClapTrap " << _name << " has no HP or energy to repair itself" << std::endl;
+		return;
+	}
+	_energyPoints -= 1;
+	_hitPoints += amount;
+	std::cout << "ClapTrap " << _name << " rapair " << amount << " HP , and have now " << _hitPoints << " HP!" << std::endl;
+}
+
---
+++ ex03/FragTrap.cpp
@@ -0,0 +1,41 @@
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap()
+{
+	_hitPoints = 100;
+	_energyPoints = 100;
+	_attackDamage = 30;
+	std::cout << "Default constructor FragTrap " << _name << " called" << std::endl;
+}
+FragTrap::FragTrap(const std::string& name) : ClapTrap(name)
+{
+	_hitPoints = 100;
+	_energyPoints = 100;
+	_attackDamage = 30;
+	std::cout << "Parametrized constructor FragTrap " << _name << " called" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other)
+{
+	std::cout << "Copy constructor FragTrap " << _name << " called" << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& other)
+{
+	std::cout << "Copy assignment FragTrap operator " << _name << " called" << std::endl;
+	if (this != &other)
+	{
+		ClapTrap::operator=(other);
+	}
+	return *this;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "Destructor FragTrap " << _name << " called" << std::endl;
+}
+
+void FragTrap::highFivesGuys(void)
+{
+	std::cout << "FragTrap " << _name << " says: Give me a high five, guys!" << std::endl;
+}
